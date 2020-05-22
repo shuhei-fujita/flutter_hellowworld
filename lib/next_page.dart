82774febe8cf570e7c4420ec07b737783d9f89ea
syuheifujita <syuheifujita0117@gmail.com>
2020-05-22 09:54:32 +0900

    add TextFild 入力フォームの実装

--- lib/next_page.dart
+++ lib/next_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutterapp/main.dart';
 
 class NextPage extends StatelessWidget {
   NextPage(this.name);
   final String name;
+  String tf_Id;
+
+  final myFocusNode = FocusNode();
+  final myController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -18,10 +21,54 @@ class NextPage extends StatelessWidget {
       ),
       body: Container(
         height: double.infinity,
-        color: Colors.indigoAccent,
+//        color: Colors.indigoAccent,
         child: Column(
           children: <Widget>[
             Text(name),
+            Column(
+              children: <Widget>[
+                TextFormField(
+                  autofocus: true,
+                  controller: myController,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'ID'
+                  ),
+                  onChanged: (text) {
+                    print("First text field: $text");
+                    tf_Id = text;
+                  },
+                ),
+                TextFormField(
+                  focusNode: myFocusNode,
+                  decoration: InputDecoration(
+                    hintText: 'password'
+                  ),
+                ),
+                RaisedButton(
+                  child: Text('フォーカス！！'),
+                  onPressed: () {
+                    myFocusNode.requestFocus();
+                  },
+                ),
+                RaisedButton(
+                  child: Text('Sign in'),
+                  onPressed: () {
+                    print("Sing in : ${myController.text}");
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          // Retrieve the text the user has entered by using the
+                          // TextEditingController.
+                          content: Text(tf_Id),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
             Center(
               child: RaisedButton(
                 child: Text('BACK'),
